d30782d3be22e7092c97bdb72aaf486d1c6f9843
liyifei <liyifei@le.com>
2017-02-06 16:21:19 +0800

    add config

---
+++ config/default.js
@@ -0,0 +1,10 @@
+// 默认配置文件
+module.exports = {
+    port: 3000,
+    session: {
+        secret: 'nodeblog',
+        key: 'nodeblog',
+        maxAge: 2592000000
+    },
+    mongodb: 'mongodb://localhost:27017/nodeblog'
+};
